50b4f673db2bcec01e8de16160b1a87e4092a48f
Michal Bradac <m.bradac.michal@gmail.com>
2021-02-03 00:28:10 +0100

    add basic flag validation

--- src/main.go
+++ src/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	operation = flag.String("o", "get", "Operation on a file, available operation: get, set")
+	query     = flag.String("q", "", "Query for selecting keys")
+	fileName  = flag.String("f", "", "File to process")
+	structure = flag.String("s", "json", "How is the file structured, available structures: json, yaml")
+)
+
 func main() {
-	
+	flag.Parse()
+	if *operation != "get" && *operation != "set" {
+		fmt.Println("Incorrect operation:", *operation)
+		flag.Usage()
+		return
+	}
+
+	if *structure != "json" && *structure != "yaml" {
+		fmt.Println("Incorrect structure:", *structure)
+		flag.Usage()
+		return
+	}
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fileReader := bufio.NewReader(file)
 }
