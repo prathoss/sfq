39cc0df35768b25411cb4be8e2b5e1a3af4ea66b
Michal Bradac <m.bradac.michal@gmail.com>
2021-02-04 18:11:16 +0100

    add parsing

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -27,12 +28,71 @@ func main() {
 		flag.Usage()
 		return
 	}
+	var parser parser
+	if *structure == "json" {
+		parser = &jsonParser{symbol: false, key: false}
+	} else {
+		parser = &yamlParser{}
+	}
 
 	file, err := os.Open(*fileName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer file.Close()
 
 	fileReader := bufio.NewReader(file)
+
+	for {
+		buffer := make([]byte, 16*1024)
+
+		length, err := fileReader.Read(buffer)
+		buffer = buffer[:length]
+
+		if length == 0 {
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+				return
+			}
+		}
+
+		text := string(buffer)
+		parser.parse(text)
+	}
+}
+
+type parser interface {
+	parse(string)
+}
+
+type jsonParser struct {
+	symbol bool
+	key    bool
+}
+
+func (parser *jsonParser) parse(text string) {
+	for _, runeVal := range text {
+		char := string(runeVal)
+		if char == " " || char == "\n" {
+			continue
+		}
+		if char == "\"" {
+			parser.symbol = !parser.symbol
+			fmt.Println()
+			continue
+		}
+		fmt.Print(char)
+	}
+}
+
+type yamlParser struct {
+}
+
+func (parser *yamlParser) parse(text string) {
+	fmt.Println("Parsing yaml")
 }
